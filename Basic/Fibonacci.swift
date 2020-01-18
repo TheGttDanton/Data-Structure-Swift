a2b40b3dd146d239be515cda55d0b2bc7431f26f
Emtiyaj Ali <emtiyajali.nitjsr@gmail.com>
2020-01-18 10:49:01 +0530

    Completed first program

--- Basic/Fibonacci.swift
+++ Basic/Fibonacci.swift
@@ -1,3 +1,21 @@
 import Foundation
 
-print("Hello")
+// Function to print fibonnaci number series upto certain range.
+
+// Method 1
+func listFibonnaciSeries(upto range: UInt) {
+    var firstNumber = 0
+    var secondNumber = 1
+    var number = 0
+    
+    print("\(number)")
+    number = secondNumber
+    
+    for _ in 1..<range {
+        print("\(number)")
+        number = firstNumber + secondNumber
+        firstNumber = secondNumber
+        secondNumber = number
+    }
+}
+listFibonnaciSeries(upto: 34)
